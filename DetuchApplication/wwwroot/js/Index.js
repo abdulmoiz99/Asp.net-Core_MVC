88da8ad76395843719de0235a18e03ed487220a7
moiz <moiza0799@gmail.com>
2019-12-19 10:51:27 +0500

    Task 32

---
+++ DetuchApplication/wwwroot/js/Index.js
@@ -0,0 +1,22 @@
+﻿
+
+console.log("Hello Abdul Moiz");
+var theForm =$("#theForm");
+theForm.hide();
+
+var button = $("buyButton");
+button.on("click", function () {
+    console.log("Thanks for Buying");
+});
+
+var productInfo = $(".product-props li");
+productInfo.on("click", function () {
+    console.log("You Clicked On " + $(this).text());
+});
+
+var $loginToggle = $("#loginToggle");
+var $popupForm = $(".popup-form");
+
+$loginToggle.on("click", function () {
+    $popupForm.fadeToggle(1000)
+});
