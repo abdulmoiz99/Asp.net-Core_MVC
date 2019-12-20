26f11cba251d0eaf3bf0272a6f3ae0eb90df2b13
moiz <moiza0799@gmail.com>
2019-12-20 18:41:51 +0500

    Task 50 : Creating Entities

---
+++ DetuchApplication/Data/DutchContext.cs
@@ -0,0 +1,15 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DetuchApplication.Data.Entities
+{
+    public class DutchContext :DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
--- DetuchApplication/ViewModels/ContactViewModel.cs
+++ DetuchApplication/ViewModels/ContactViewModel.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace DetuchApplication.ViewModels
 {
+    
     public class ContactViewModel
     {
         [Required]
